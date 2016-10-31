059e2c367c89da8445ca5fd0ceb846dad9d1f8cd
sopermaf <sopermaf@tcd.ie>
2016-10-31 06:45:47 -0700

    recieves information add uppercase text, could be improved to show just uppercase text in response

--- Lab 1/echo.py
+++ Lab 1/echo.py
@@ -4,8 +4,11 @@ s = socket.socket()         # Create a socket object
 host = "localhost"	    # On local host this time
 port = 8000                 # Correct Port Number Required
 
-s.connect((host, port))
-s.send("echo-server.php?message=test")
 
-print s.recv(10)
+message = "GET /echo-server.php?message=hello HTTP/1.1\r\n\r\n"
+
+s.connect((host, port))
+s.send(message)
+
+print s.recv(496)
 s.close
