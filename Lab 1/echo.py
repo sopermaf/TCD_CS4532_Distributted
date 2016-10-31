6a84ed7d453d59fbdf41915ba987d29db181f5c6
sopermaf <sopermaf@tcd.ie>
2016-10-31 06:56:31 -0700

    variable addition for input string

--- Lab 1/echo.py
+++ Lab 1/echo.py
@@ -4,11 +4,11 @@ s = socket.socket()         # Create a socket object
 host = "localhost"	    # On local host this time
 port = 8000                 # Correct Port Number Required
 
-
-message = "GET /echo-server.php?message=hello HTTP/1.1\r\n\r\n"
+aim_message = "ferdia"
+message = "GET /echo-server.php?message=" + aim_message + " HTTP/1.1\r\n\r\n"
 
 s.connect((host, port))
 s.send(message)
 
-print s.recv(496)
+print s.recv(256)
 s.close
