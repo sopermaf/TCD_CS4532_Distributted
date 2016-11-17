1a7fc5f84c39caf64df43139d42efd735217d45c
sopermaf <sopermaf@tcd.ie>
2016-11-17 06:52:15 -0800

    placeholder comments for future development of thread pooling

--- lab 2/server.py
+++ lab 2/server.py
@@ -1,10 +1,21 @@
 import socket
-import thread
-
-#create TCP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('localhost', 8003))
-sock.listen(1)
+import threading
+	
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        
+    def run(self):
+        print "Starting " + self.name
+        #add logic for thread waiting for instructions here
+        while True:
+        	#lock resource
+        	#change needed variable
+        	#release resource
+        	#deal with client
 
 def newClient(connection, client_address):
 	print 'connection from: ' + str(client_address)
@@ -18,10 +29,18 @@ def newClient(connection, client_address):
 	print 'closing connection\n'
 	connection.close()
 
+#our threadpool setup
+threadpool = [] 
+
+#create TCP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('localhost', 8005))
+sock.listen(1)
+
 while True:
     # Wait for a connection
     print 'waiting for a connection'
     
     connection, client_address = sock.accept()
-    thread.start_new_thread(newClient, (connection, client_address))  
+    #thread.start_new_thread(newClient, (connection, client_address))  
     
