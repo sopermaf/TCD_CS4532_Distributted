0cc215cb439b9d9f25ee17ed6a3a4152a89a60df
sopermaf <sopermaf@tcd.ie>
2016-11-17 06:06:12 -0800

    server now starts new thread for each client

--- lab 2/echo.py
+++ lab 2/echo.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket()         # Create a socket object
 host = "localhost"	    # On local host this time
-port = 8002                 # Correct Port Number Required
+port = 8003	            # Correct Port Number Required
 
 aim_message = "ferdia"
 message = "hello"
--- lab 2/server.py
+++ lab 2/server.py
@@ -1,23 +1,27 @@
 import socket
-import sys
+import thread
 
 #create TCP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('localhost', 8002))
+sock.bind(('localhost', 8003))
 sock.listen(1)
+
+def newClient(connection, client_address):
+	print 'connection from: ' + str(client_address)
+
+	inData = connection.recv(256)
+	print 'received: ' + str(inData)
+
+	connection.sendall(inData)
+	print 'sending back: ' + str(inData)
+
+	print 'closing connection\n'
+	connection.close()
 
 while True:
     # Wait for a connection
     print 'waiting for a connection'
     
-    connection, client_address = sock.accept()   
-    print 'connection from: ' + str(client_address)
+    connection, client_address = sock.accept()
+    thread.start_new_thread(newClient, (connection, client_address))  
     
-    inData = connection.recv(256)
-    print 'received: ' + str(inData)
-    
-    connection.sendall(inData)
-    print 'sending back: ' + str(inData)
-    
-    print 'closing connection\n'
-    connection.close()
