7d795ab7b1cea8cf0676ee59367daea4e2c5e0d8
sopermaf <sopermaf@tcd.ie>
2016-11-20 07:43:26 -0800

    working thread pool implementation

--- lab 2/echo.py
+++ lab 2/echo.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket()         # Create a socket object
 host = "localhost"	    # On local host this time
-port = 8003	            # Correct Port Number Required
+port = 8007	            # Correct Port Number Required
 
 aim_message = "ferdia"
 message = "hello"
--- lab 2/server.py
+++ lab 2/server.py
@@ -1,6 +1,11 @@
 import socket
 import threading
-	
+
+#shared data setup
+connections = []
+client_addr = []
+connectLock = threading.Condition()
+
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
@@ -9,38 +14,55 @@ class myThread (threading.Thread):
         self.counter = counter
         
     def run(self):
-        print "Starting " + self.name
-        #add logic for thread waiting for instructions here
+        print "Starting " + self.name        
         while True:
-        	#lock resource
-        	#change needed variable
-        	#release resource
-        	#deal with client
+            #lock for concurrency
+            with connectLock:
+                while len(connections) < 1: #no connections to reply to
+                    #print str(self.threadID) + " waiting for data"
+                    connectLock.wait()
+                #copy the data to local version
+                copy_connect = connections[0]
+                copy_addr = client_addr[0]
+                connections.pop(0)
+                client_addr.pop(0)
 
-def newClient(connection, client_address):
-	print 'connection from: ' + str(client_address)
+            #lock not needed 
+            newClient(copy_connect, copy_addr, self.threadID)
+        
 
-	inData = connection.recv(256)
-	print 'received: ' + str(inData)
-
-	connection.sendall(inData)
-	print 'sending back: ' + str(inData)
-
-	print 'closing connection\n'
-	connection.close()
+def newClient(connection, client_address, threadID):
+    print 'connection from: ' + str(client_address) + " -- processing on thread " + str(threadID)
+    inData = connection.recv(256)
+    print 'received: ' + str(inData)
+    connection.sendall(inData)
+    print 'sending back: ' + str(inData)
+    print 'closing connection\n'
+    connection.close()
 
 #our threadpool setup
-threadpool = [] 
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+thread1.start()
+thread2.start()
+
 
 #create TCP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('localhost', 8005))
+sock.bind(('localhost', 8007))
 sock.listen(1)
 
 while True:
     # Wait for a connection
     print 'waiting for a connection'
+    new_connect, new_addr = sock.accept()
     
-    connection, client_address = sock.accept()
-    #thread.start_new_thread(newClient, (connection, client_address))  
+    with connectLock:
+    	#append new collection to the end
+    	connections.append(new_connect)
+    	client_addr.append(new_addr)
+    	#notify a thread
+    	connectLock.notify()
+    
+     
     
