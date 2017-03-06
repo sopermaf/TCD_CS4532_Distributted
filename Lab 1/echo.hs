cadb688990aa08c9c623787548e5e8d1034e0ab7
sopermaf <sopermaf@tcd.ie>
2017-03-06 16:57:57 +0000

    working haskell solution

--- Lab 1/echo.hs
+++ Lab 1/echo.hs
@@ -1,5 +1,19 @@
-import Network.HTTP.*
+import Network
+import System.Environment
+import System.IO
+import Network.HTTP
+import Data.List.Split
 
-simpleHTTP (getRequest "http://www.haskell.org/") >>= fmap (take 100) . getResponseBody
+main :: IO ()
+main = do
+    [port, message] <- getArgs
+    tcpHandler <- connectTo "localhost" $ PortNumber $ toEnum $ read port
+    hSetBuffering tcpHandler  LineBuffering
+    hPutStr tcpHandler $ buildHTTPMessage message
+    hGetContents tcpHandler >>= putStr . parseResponse 
 
+buildHTTPMessage :: String -> String
+buildHTTPMessage msg = "GET /echo-server.php?message=" ++ msg ++ " HTTP/1.1\r\n\r\n"
 
+parseResponse :: String -> String
+parseResponse httpMsg = "Response:\n" ++ last (splitOn "\r\n\r\n" httpMsg) ++ "\n"
